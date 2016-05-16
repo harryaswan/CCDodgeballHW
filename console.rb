7fd86511fc4c53f898545d64e87206b79642e7de
Harry A. Swan <has.land@hotmail.co.uk>
2016-05-16 06:47:14 +0100

    tidy up

--- console.rb
+++ console.rb
@@ -17,19 +17,19 @@ teams << Team.new({'name'=>'Devil Dogs', 'location'=>'Texas'}).save()
 teams << Team.new({'name'=>'Hard Throwers', 'location'=>'Florida'}).save()
 
 players = []
-players << Player.new({'name'=>'Player1', 'team_id'=>team1.id}).save()
-players << Player.new({'name'=>'Player2', 'team_id'=>team1.id}).save()
-players << Player.new({'name'=>'Player3', 'team_id'=>team2.id}).save()
-players << Player.new({'name'=>'Player4', 'team_id'=>team2.id}).save()
-players << Player.new({'name'=>'Player5', 'team_id'=>team3.id}).save()
-players << Player.new({'name'=>'Player6', 'team_id'=>team3.id}).save()
+players << Player.new({'name'=>'Player1', 'team_id'=>teams[0].id}).save()
+players << Player.new({'name'=>'Player2', 'team_id'=>teams[0].id}).save()
+players << Player.new({'name'=>'Player3', 'team_id'=>teams[1].id}).save()
+players << Player.new({'name'=>'Player4', 'team_id'=>teams[1].id}).save()
+players << Player.new({'name'=>'Player5', 'team_id'=>teams[2].id}).save()
+players << Player.new({'name'=>'Player6', 'team_id'=>teams[2].id}).save()
 
 coaches = []
-coaches << Coach.new({'name'=>'Dave', 'team_id'=>team1.id}).save()
-coaches << Coach.new({'name'=>'Colin', 'team_id'=>team2.id}).save()
-coaches << Coach.new({'name'=>'James', 'team_id'=>team3.id}).save()
+coaches << Coach.new({'name'=>'Dave', 'team_id'=>teams[0].id}).save()
+coaches << Coach.new({'name'=>'Colin', 'team_id'=>teams[1].id}).save()
+coaches << Coach.new({'name'=>'James', 'team_id'=>teams[2].id}).save()
 
-league = League.new([team1, team2, team3])
+league = League.new(teams)
 
 
 binding.pry
